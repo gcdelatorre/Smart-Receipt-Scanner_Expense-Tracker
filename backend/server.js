e3675c507f5504e6368afe215e9e4dce869ce0ea
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2025-12-04 13:09:38 +0800

    fixed upload route

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const __dirname = path.dirname(__filename);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, "uploads/"),
+    destination: (req, file, cb) => cb(null, path.join(__dirname, "uploads")),
     filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname)
 });
 
