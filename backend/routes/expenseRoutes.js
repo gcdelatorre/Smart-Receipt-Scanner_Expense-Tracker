56b04c93a194bfa60d8c93a8e444cc2c01cd45a1
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2025-12-06 02:10:34 +0800

    fix

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import { addExpense, getAll } from "../controllers/expenseController.js";
-import { uploadExpense, upload } from "../controllers/uploadController.js";
+import { createExpenseWithUpload, upload } from "../controllers/uploadController.js";
 
 const router = express.Router()
 
 router.get("/", getAll)
 router.post("/", addExpense)
-router.post("/upload", upload.single("image"), uploadExpense)
+router.post("/upload", upload.single("image"), createExpenseWithUpload)
 
 export default router
