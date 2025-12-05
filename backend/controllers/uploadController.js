b1048fdf2e48b590712cea336f1dba5dd13ea897
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2025-12-06 02:10:27 +0800

    implemented automatic expense create

--- backend/controllers/uploadController.js
+++ backend/controllers/uploadController.js
@@ -2,6 +2,7 @@ import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { extractTextFromImage, categorizeExpense } from "../services/gemini.js";
+import { createExpense } from '../services/createExpense.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,19 +14,21 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-export const uploadExpense = async (req, res) => {
+export const createExpenseWithUpload = async (req, res) => {
     try {
         const filePath = req.file.path;
 
         const text = await extractTextFromImage(filePath);
         const structured = await categorizeExpense(text);
 
-
+        const newExpense = await createExpense({ ...structured, imageUrl: `/uploads/${req.file.filename}`, userId: "64c1f0f9a4f12b3a5e123456"}) 
+                                                                                                            // hardcoded userId for now
+                                                                                                            // will add userId after i added user models or user accounts
+                                                                                                            // and with authentication to input valid id
+                                                                                                            // so that it can determine whose this expense if its theirs
         return res.json({
             success: true,
-            receiptText: text,
-            extracted: structured,
-            imageUrl: `/uploads/${req.file.filename}`
+            data: newExpense
         });
 
     } catch (err) {
