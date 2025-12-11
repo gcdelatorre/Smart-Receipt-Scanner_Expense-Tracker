1eed2dcac5ccc472d3817a94a71c459c352e8a83
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2025-12-12 01:40:02 +0800

    display overall budget

--- frontend/src/components/dashboard/BudgetsCard.jsx
+++ frontend/src/components/dashboard/BudgetsCard.jsx
@@ -1,12 +1,29 @@
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Progress } from "../ui/progress";
 import { Info } from "lucide-react";
+import { fetchUserBudget } from '../utils/fetchUser'
+import { useEffect, useState } from "react";
 
 export default function BudgetsCard({ budgets }) {
+
+  const [overallBudget, setOverallBudget] = useState(0)
+
+  useEffect(() => {
+    const getBudget = async () => {
+
+      let budget = await fetchUserBudget()
+      setOverallBudget(budget)
+    }
+    getBudget()
+  }, [])
+
   return (
     <Card className="lg:col-span-4">
       <CardHeader>
-        <CardTitle className="text-xl font-semibold text-slate-900">Budgets</CardTitle>
+        <CardTitle className="text-sm font-medium text-slate-500">Budgets</CardTitle>
+        <div className="mt-3 flex items-center gap-2 text-2xl font-semibold text-slate-900">
+          {overallBudget ? `$${overallBudget.toFixed(2)}` : `Loading...`}
+        </div>
         <button className="text-sm font-medium text-indigo-600 hover:text-indigo-700">
           Edit
         </button>
