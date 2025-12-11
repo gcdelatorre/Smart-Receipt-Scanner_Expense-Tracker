4b707ed55b26d166a053c21cc8bc2d22f2de2cc0
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2025-12-11 20:23:58 +0800

    create user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: false },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    overallBudget: { type: Number, default: 0 },
+    categoryBudgets: {
+        type: Array,
+        default: []
+    }
+}, { timestamps: true });
+
+export default mongoose.model("User", userSchema);
