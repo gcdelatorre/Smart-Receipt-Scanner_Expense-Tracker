6932ab78a0739956961d31b4e5c5a1e141cdf48e
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2025-12-12 19:20:50 +0800

    update fetch user

--- frontend/src/components/utils/fetchUser.js
+++ frontend/src/components/utils/fetchUser.js
@@ -1,5 +1,9 @@
 export const fetchUserBudget = async () => {
     const res = await fetch("/api/user/693aec9c08d1f6edd4c2ad5f").then(r => r.json());
-    return res.data.overallBudget;
+
+    return {
+        overallBudget: res.data.overallBudget,
+        categoryBudgets: res.data.categoryBudgets
+    };
 };
 
