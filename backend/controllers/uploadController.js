a7ab267202254f9bcab8f213403a541e270e51af
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2025-12-13 03:54:31 +0800

    added updating usedAmount when creating expense through receipt

--- backend/controllers/uploadController.js
+++ backend/controllers/uploadController.js
@@ -3,6 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { extractTextFromImage, categorizeExpense } from "../services/gemini.js";
 import { createExpense } from '../services/expenseService.js';
+import User from '../models/user.model.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,12 +17,43 @@ const upload = multer({ storage });
 
 export const createExpenseFromReceipt = async (req, res) => {
     try {
+
         const filePath = req.file.path;
 
         const text = await extractTextFromImage(filePath);
         const structured = await categorizeExpense(text);
 
-        const newExpense = await createExpense({ ...structured, imageUrl: `/uploads/${req.file.filename}`, userId: "64c1f0f9a4f12b3a5e123456" })
+
+
+        const expenseAmount = Number(structured.amount);
+        const category = structured.category;
+        const user = await User.findById("693aec9c08d1f6edd4c2ad5f");
+        if (!user) return res.status(404).json({ success: false, message: "User Not Found" });
+
+        const categoryBudget = user.categoryBudgets.find(cat => cat.category === category);
+
+        if (!categoryBudget) {
+            return res.status(400).json({
+                success: false,
+                message: `No budget found for category: ${category}`
+            });
+        }
+
+        if (categoryBudget.usedAmount + expenseAmount > categoryBudget.amount) {
+            return res.status(400).json({
+                success: false,
+                message: `Expense exceeds budget for category: ${category}`
+            });
+        }
+
+        await User.updateOne(
+            { _id: "693aec9c08d1f6edd4c2ad5f", "categoryBudgets.category": category },
+            { $inc: { "categoryBudgets.$.usedAmount": expenseAmount } }
+        );
+
+
+
+        const newExpense = await createExpense({ ...structured, imageUrl: `/uploads/${req.file.filename}`, userId: "693aec9c08d1f6edd4c2ad5f" })
         // hardcoded userId for now
         // will add userId after i added user models or user accounts
         // and with authentication to input valid id
