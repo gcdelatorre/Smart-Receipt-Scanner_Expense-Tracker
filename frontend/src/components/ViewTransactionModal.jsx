5f580be02a47ff0a0755243377331894778d5495
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2025-12-14 21:40:35 +0800

    updated transaction modal, conditional render income and expense, rendered datas

--- frontend/src/components/ViewTransactionModal.jsx
+++ frontend/src/components/ViewTransactionModal.jsx
@@ -1,9 +1,19 @@
 import { Button } from "./ui/button";
-import { ArrowUpRight, ArrowDownRight } from "lucide-react";
+import { ArrowUpRight, ArrowDownRight, ReceiptText } from "lucide-react";
 
-export default function ViewTransactionModal({ open, onClose, selectedTransactionType }) {
+export default function ViewTransactionModal({ open, onClose, transactionToView }) {
+
+    const formattedDate = (dateStr) => {
+        const d = new Date(dateStr);
+        const options = { month: "short", day: "numeric", year: "numeric" };
+        return d.toLocaleDateString(undefined, options);
+    }
+
+    const formattedItems = (items) => {
+        items.map((items) => items.name).join(", ");
+    }
+
     if (!open) return null;
-
 
     // placeholder for income are, category, amount, note, date
     // placeholder for expense are, category, amount, store, items, note, date
@@ -22,38 +32,50 @@ export default function ViewTransactionModal({ open, onClose, selectedTransactio
                         </Button>
                     </div>
 
-                    {/* Transaction Info Placeholder */}
                     <div className="space-y-3">
                         <div className="flex items-center gap-3">
-                            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-slate-200 text-slate-500">
-                                <ArrowUpRight className="h-5 w-5" />
+                            <div className={`flex h-10 w-10 items-center justify-center rounded-full ${transactionToView.transactionType === "income" ? "bg-green-100" : "bg-rose-100"}`}>
+                                {transactionToView.transactionType === "income" ? <ArrowUpRight variant="success" className="h-5 w-5 text-green-500" /> : <ArrowDownRight variant="destructive" className=" text-rose-500 h-5 w-5" />}
                             </div>
                             <div className="flex flex-col gap-1">
-                                <p className="font-semibold bg-slate-100 h-4 w-32 rounded"></p>
-                                <p className="text-xs text-slate-400 bg-slate-100 h-3 w-24 rounded"></p>
+                                <p className="font-semibold text-xl">{transactionToView.transactionType === "income" ? "Income" : "Expense"}</p>
                             </div>
                         </div>
 
                         <div>
-                            <p className="text-sm font-medium text-slate-700">Amount</p>
-                            <p className="font-semibold bg-slate-100 h-5 w-20 rounded"></p>
+                            <p className="text-base font-semibold text-slate-700">Category</p>
+                            <p className="text-sm text-slate-500">{transactionToView.category || "N/A"}</p>
                         </div>
 
                         <div>
-                            <p className="text-sm font-medium text-slate-700">Store</p>
-                            <p className="bg-slate-100 h-4 w-32 rounded"></p>
+                            <p className="text-base font-semibold text-slate-700">Amount</p>
+                            <p className="text-sm text-slate-500">${transactionToView.amount.toFixed(2)}</p>
                         </div>
 
-                        <div>
-                            <p className="text-sm font-medium text-slate-700">Description</p>
-                            <p className="bg-slate-100 h-4 w-full rounded"></p>
-                        </div>
+                        {transactionToView.transactionType === "expense" && (
+                            <>
+                                <div>
+                                    <p className="text-base font-semibold text-slate-700">Store</p>
+                                    <p className="text-sm text-slate-500">{transactionToView.store || "N/A"}</p>
+                                </div>
+                                <div>
+                                    <p className="text-base font-semibold text-slate-700">Items</p>
+                                    <p className="text-sm text-slate-500">{formattedItems(transactionToView.items) || "N/A"}</p>
+                                </div>
+                            </>
+                        )}
 
                         <div>
                             <p className="text-sm font-medium text-slate-700">Note</p>
-                            <p className="bg-slate-100 h-4 w-full rounded"></p>
+                            <p className="text-sm text-slate-500">{transactionToView.description || "N/A"}</p>
+                        </div>
+
+                        <div>
+                            <p className="text-base font-semibold text-slate-700">Date</p>
+                            <p className="text-sm text-slate-500">{formattedDate(transactionToView.date)}</p>
                         </div>
                     </div>
+
 
                     {/* Footer */}
                     <div className="mt-6 flex justify-end gap-2">
