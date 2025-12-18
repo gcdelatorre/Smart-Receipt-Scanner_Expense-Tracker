39d86dd01dd4f398cf847f44682b3b304c9790f5
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2025-12-18 17:08:32 +0800

    create category selection

---
+++ frontend/src/components/CategorySelection.jsx
@@ -0,0 +1,38 @@
+import {
+    Select,
+    SelectGroup,
+    SelectValue,
+    SelectTrigger,
+    SelectContent,
+    SelectLabel,
+    SelectItem,
+} from "./ui/select"
+
+export default function CategorySelection({ categories = [], onChange, name }) {
+
+    const categoryElements = categories.map(category => {
+        return (
+            <SelectItem key={category} value={category}>
+                {category}
+            </SelectItem>
+        )
+    })
+
+    return (
+        <Select
+            onValueChange={(value) =>
+                onChange({ target: { name, value } })
+            }
+        >
+            <SelectTrigger>
+                <SelectValue placeholder="Select a Category" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                    <SelectLabel>Category</SelectLabel>
+                    {categoryElements}
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+    )
+}
