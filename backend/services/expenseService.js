971043927a9ac941f2459f266be0ac1b905e8b87
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2025-12-27 15:20:23 +0800

    create deduct category budget and delete expense and update budget separate function

--- backend/services/expenseService.js
+++ backend/services/expenseService.js
@@ -1,4 +1,5 @@
 import Expense from '../models/expense.model.js'
+import User from '../models/user.model.js'
 
 // create a new expense
 export const createExpense = async (expenseData) => {
@@ -19,8 +20,35 @@ export const getAll = async () => {
     return expenses;
 }
 
+
 // find expense by id
 export const findExpenseById = async (expenseId) => {
     const expense = await Expense.findById(expenseId);
     return expense;
 }
+
+export const deductCategoryBudget = async (userId, category, amount) => {
+    await User.updateOne(
+        {
+            _id: userId,
+            "categoryBudgets.category": category
+        },
+        {
+            $inc: {
+                "categoryBudgets.$.usedAmount": -amount
+            }
+        }
+    )
+}
+
+export const deleteExpenseAndUpdateBudget = async (expenseId) => {
+    const expense = await Expense.findById(expenseId)
+
+    if (!expense) {
+        throw { status: 404, message: "Expense not found" };
+    }
+
+    await expense.deleteOne();
+
+    await deductCategoryBudget(expense.userId, expense.category, expense.amount);
+}    
