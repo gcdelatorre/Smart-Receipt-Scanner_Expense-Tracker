883124fa2b3f421836e58b5d0d7e8c4682f9d074
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2026-02-02 00:47:08 +0800

    fix rate limiter

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -1,10 +1,13 @@
 import rateLimit from "express-rate-limit";
 
+// rate limiting are used to prevent abuse of the API or brute force attacks or ddos attacks
+
 export const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 10,
+    max: 25,
     message: {
-        error: "Too many requests from this IP, please try again later."
+        status: 429,
+        message: "Too many requests from this IP, please try again later."
     },
     standardHeaders: true,
     legacyHeaders: false
@@ -14,7 +17,8 @@ export const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 100,
     message: {
-        error: "Too many requests from this IP, please try again later."
+        status: 429,
+        message: "Too many requests from this IP, please try again later."
     },
     standardHeaders: true,
     legacyHeaders: false
@@ -24,7 +28,8 @@ export const uploadLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 5,
     message: {
-        error: "Too many requests from this IP, please try again later."
+        status: 429,
+        message: "Too many requests from this IP, please try again later."
     },
     standardHeaders: true,
     legacyHeaders: false
